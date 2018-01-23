31a583a54a7051aad1b83f032ef775ff1fdb40c7
suhey <suhey@DESKTOP-NFL3H7G.lan>
2018-01-23 15:24:19 -0800

    Added in test method for the encoder

--- Tshirty4579/src/org/usfirst/frc4579/Tshirty4579/commands/drive.java
+++ Tshirty4579/src/org/usfirst/frc4579/Tshirty4579/commands/drive.java
@@ -42,6 +42,7 @@ public class drive extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.driveTrain.drive(1);
+    	System.out.println(Robot.driveTrain.getRotation());
     }
 
     // Make this return true when this Command no longer needs to run execute()
